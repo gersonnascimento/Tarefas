3a5fb3c441773575dc4e9eeda5735cb6a023e2d5
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-27 17:49:09 -0200

    login

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,30 +1,34 @@
 class NotesController < ApplicationController
-	def index
+
+          http_basic_authenticate_with name: "gerson", password: "gerson"
+
+        def index
 		@notes = Note.where("status = 0")
 		render 'index'
 	end
 
 	def ver_todas
-      @notes = Note.all
-      render 'index'
+          @notes = Note.all
+          render 'index'
 	end
 
 	def finalizeds
-      @notes = Note.where("status = 1")
-      render 'index'
+          @notes = Note.where("status = 1")
+          render 'index'
 	end
 
 	def new
 		@novo = Note.new
 	end
+
 	def create
-	     @novo = Note.new params.require(:note).permit(:titulo, :texto)
-	     @novo.status = 0
-         if @novo.save then
-         redirect_to root_url
-         else
-         redirect_to "/produtos/new"
-        end
+          @novo = Note.new params.require(:note).permit(:titulo, :texto)
+	  @novo.status = 0
+          if @novo.save then
+            redirect_to root_url
+          else
+            redirect_to "/produtos/new"
+          end
 	end
 
 	def show
@@ -36,17 +40,16 @@ class NotesController < ApplicationController
 	end
 
 	def update
-      titulo = params[:note][:titulo]
-      texto= params[:note][:texto]
-
-      @selected = Note.find(params[:id])
-      @selected.update(titulo: titulo, texto:texto)
-      redirect_to note_path(@selected.id)
+          titulo = params[:note][:titulo]
+          texto= params[:note][:texto]
+          @selected = Note.find(params[:id])
+          @selected.update(titulo: titulo, texto:texto)
+          redirect_to note_path(@selected.id)
 	end
 
 	def finalize
-      @selected = Note.find(params[:id])
-      @selected.update(status: 1)
-      redirect_to root_path
+          @selected = Note.find(params[:id])
+          @selected.update(status: 1)
+          redirect_to root_path
 	end
 end
